99cda689f4c6f86c7853d5c8ab0cc01e23049a77
vaishnavav99 <vaishnavav96@gmail.com>
2021-05-16 12:13:18 +0530

    push1

---
+++ music/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+#import ffmpeg-python
+import secrets
+
+app= Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert',methods=['POST'])
+def convert():
+    file=request.files['input']
+    x= secrets.token_hex(20)+'.mp3'
+    filePath = "./file/"+secure_filename(x)
+    file.save(filePath)
+    return "Sucess"
+
+if __name__=='__main__':
+    app.run(debug=True)
